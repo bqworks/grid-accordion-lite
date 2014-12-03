bc199b266a348d29d9a6e09fb365d47f1f041a00
davidghi <david.ghiurau@gmail.com>
2014-12-03 15:51:43 +0200

    update price in readme.txt

--- admin/views/upgrade.php
+++ admin/views/upgrade.php
@@ -2,7 +2,7 @@
 	<h2><?php _e( 'Upgrade to Full Version', 'grid-accordion' ); ?></h2>
     <p>If you need more features or simply want to support the development of the Grid Accordion plugin, you can upgrade to the full version.</p>
     
-    <p><a href="http://codecanyon.net/item/grid-accordion-responsive-wordpress-plugin/7788530?ref=bqworks">Buy the full version for only $16 &rarr;</a></p>
+    <p><a href="http://codecanyon.net/item/grid-accordion-responsive-wordpress-plugin/7788530?ref=bqworks">Buy the full version for only $18 &rarr;</a></p>
     
     <p>Some of the available features are:</p>
     <ul>
@@ -21,5 +21,5 @@
     <p>You can see some examples and read more about the available features at <a href="http://bqworks.com/grid-accordion/">http://bqworks.com/grid-accordion/</a>.</p>
     <p>Also, you can check out the <a href="https://www.youtube.com/watch?v=XqeHLv052Bc&list=PLh-6IaZNuPo7Skwefhb9T2CSazDjC56Lg">video tutorials</a> to understand better how the plugin works.</p>
     
-    <p><a href="http://codecanyon.net/item/grid-accordion-responsive-wordpress-plugin/7788530?ref=bqworks">Buy the full version for only $16 &rarr;</a></p>
+    <p><a href="http://codecanyon.net/item/grid-accordion-responsive-wordpress-plugin/7788530?ref=bqworks">Buy the full version for only $18 &rarr;</a></p>
 </div>
